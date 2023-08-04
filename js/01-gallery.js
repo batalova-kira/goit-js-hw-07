5feff4a8b47f7ff2b5bf014e237cc189bdfe5224
batalova-kira <batalovakira@gmail.com>
2023-08-04 07:43:59 +0300

    Task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const container = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+    return arr.map(({preview, original, description}) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('')
+}
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+container.addEventListener('click', handlerGalleryClick);
+
+function handlerGalleryClick(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;
+    }
+    const imgLagre = evt.target.dataset.source;
+    const instance = basicLightbox.create(`<img src="${imgLagre}"/>
+`)
+    instance.show();
+}
+
+document.addEventListener('keydown', function (evt) {
+  if (evt.code === "Escape") {
+      evt.preventDefault();
+    instance.close();
+}
+});
 
 console.log(galleryItems);
